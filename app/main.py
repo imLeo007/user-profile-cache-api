adca71470827b1a8c1e13e4d2b477db1e568deb7
AKUMARTHI MARIYA RAJU <r01979818@gmail.com>
2026-07-22 12:36:06 +0530

    Prepare API for Render Deployment

--- app/main.py
+++ app/main.py
@@ -5,3 +5,16 @@ from app.routers.user import router as user_router
 app = FastAPI()
 
 app.include_router(user_router)
+
+
+@app.get("/")
+async def root() -> dict:
+    return {
+        "message": "User Profile Cache API is Running"
+    }
+
+@app.get("/health")
+async def health_check() -> dict:
+    return {
+        "status": "healthy"
+    }
